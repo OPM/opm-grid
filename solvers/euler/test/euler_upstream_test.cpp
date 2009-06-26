959bbb249ee2a9bd40333a3ab94d865b06c20ad4
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2009-06-26 15:11:57 +0000

    Working on euler test case.

--- solvers/euler/test/euler_upstream_test.cpp
+++ solvers/euler/test/euler_upstream_test.cpp
@@ -14,29 +14,69 @@
 //===========================================================================
 
 /*
-Copyright 2009 SINTEF ICT, Applied Mathematics.
-Copyright 2009 Statoil ASA.
+  Copyright 2009 SINTEF ICT, Applied Mathematics.
+  Copyright 2009 Statoil ASA.
 
-This file is part of The Open Reservoir Simulator Project (OpenRS).
+  This file is part of The Open Reservoir Simulator Project (OpenRS).
 
-OpenRS is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  OpenRS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-OpenRS is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  OpenRS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#include "config.h"
 #include "../EulerUpstream.hpp"
+#include "../GridInterfaceEuler.hpp"
+#include <dune/grid/yaspgrid.hh>
+#include <dune/common/mpihelper.hh>
 
-int main()
+template <int dim>
+void initYaspGrid(Dune::YaspGrid<dim>& grid)
 {
+    typedef Dune::FieldVector<int,dim> iTuple;
+    typedef Dune::FieldVector<double,dim> fTuple;
+    typedef Dune::FieldVector<bool,dim> bTuple;
+    fTuple cell_sz(1.0);
+    iTuple dims(3);
+    bTuple periodic(false);
+    int overlap = 1;
+    grid = Dune::YaspGrid<dim>(cell_sz, dims, periodic, overlap);
+    grid.globalRefine(2);
+}
+
+
+int main(int argc, char** argv)
+{
+    Dune::parameter::ParameterGroup param(argc, argv);
+    Dune::MPIHelper::instance(argc,argv);
+
+    // Make a grid.
+    const int dim = 3;
+    typedef Dune::YaspGrid<dim> GridType;
+    typedef Dune::FieldVector<int,dim> iTuple;
+    typedef Dune::FieldVector<double,dim> fTuple;
+    typedef Dune::FieldVector<bool,dim> bTuple;
+    fTuple cell_sz(1.0);
+    iTuple dims(3);
+    bTuple periodic(false);
+    int overlap = 1;
+    Dune::YaspGrid<dim> grid(cell_sz, dims, periodic, overlap);
+    grid.globalRefine(2);
+
+    // Make a solver.
     Dune::EulerUpstream transport_solver;
+
+    // Make the grid interface
+    Dune::GridInterfaceEuler<GridType> g(grid);
+
+    //transport_solver.transportSolve(sat, time, resdata, boundary, face_fluxes);
 }
 
