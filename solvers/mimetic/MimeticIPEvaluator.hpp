e3395acdaa65770664e1a9e6fc9ace8bb5a31f0d
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2009-06-29 09:11:07 +0000

    Correct various minor (and not-so-minor) mistakes relating to matrix   sizes.  In particular, the T1 and T2 matrices are nf-by-dim, not   nf-by-nf.
      Clear residual data from all matrices, not just Binv.
    
      Accept the world dimension as a template parameter.
    
      Chase name change 'MxM' -> 'matMulAdd'.

--- solvers/mimetic/MimeticIPEvaluator.hpp
+++ solvers/mimetic/MimeticIPEvaluator.hpp
@@ -36,79 +36,90 @@
 #ifndef OPENRS_MIMETICIPEVALUATOR_HEADER
 #define OPENRS_MIMETICIPEVALUATOR_HEADER
 
-#include <algorithm>   // for std::fill_n()
 #include <vector>
+
+#include <dune/common/ErrorMacros.hpp>
 
 #include <dune/solvers/mimetic/fortran.hpp>
 #include <dune/solvers/mimetic/blas_lapack.hpp>
 #include <dune/solvers/mimetic/FortranMatrix.hpp>
 
 namespace Dune {
-    template<class CellIter, bool computeInverseIP> class MimeticIPEvaluator;
+    template<class CellIter, int dim, bool computeInverseIP> class MimeticIPEvaluator;
 
-    template<class CellIter>
-    class MimeticIPEvaluator<CellIter,true> {
+    template<class CellIter, int dim>
+    class MimeticIPEvaluator<CellIter,dim,true> {
     public:
         typedef typename CellIter::Scalar Scalar;
 
         MimeticIPEvaluator(const int max_nf)
             : max_nf_(max_nf),
               work_  (64 * max_nf),  // 64 from ILAENV
-              tau_   (3),            // Magic (only for <= 3D)
+              tau_   (dim),
               fa_    (max_nf * max_nf),
-              t1_    (max_nf * max_nf),
-              t2_    (max_nf * max_nf)
+              t1_    (max_nf * dim),
+              t2_    (max_nf * dim)
         {}
 
-        void evaluate(const CellIter& c, const std::vector<T>& perm,
-                      FortranMatrix<Scalar>& Binv)
+
+        void evaluate(const CellIter&            c,
+                      const std::vector<Scalar>& perm,
+                      FortranMatrix<Scalar>&     Binv)
         {
             typedef typename CellIter::FaceIterator FI;
             typedef typename CellIter::Vector       CV;
             typedef typename FI      ::Vector       FV;
 
+            ASSERT(Binv.numRows() <= max_nf_);
+            ASSERT(Binv.numRows() == Binv.numCols());
+            ASSERT(FV::size       == dim);
+            ASSERT(t1_.size()     >= nf * dim);
+            ASSERT(t2_.size()     >= nf * dim);
+            ASSERT(fa_.size()     >= nf * nf);
+
             const int nf = Binv.numRows();
-            const CV  cc = c.centroid();
-            const int nd = FV::size;    ASSERT(perm.size() == nd*nd);
 
-            FortranMatrix<Scalar,false> T1(nf, nf, &t1_[0]);
-            FortranMatrix<Scalar,false> T2(nf, nf, &t2_[0]);
-            FortranMatrix<Scalar,false> FA(nf, nf, &fa_[0]);
+            FortranMatrix<Scalar,false> T1(nf, dim, &t1_[0]);
+            FortranMatrix<Scalar,false> T2(nf, dim, &t2_[0]);
+            FortranMatrix<Scalar,false> fa(nf, nf , &fa_[0]);
 
-            // Clear input matrix of any residual data.
-            std::fill_n(Binv.data(), nf * nf, Scalar(0.0));
+            // Clear matrices of any residual data.
+            zero(Binv);  zero(T1);   zero(T2);   zero(fa);
 
+            // Setup: Binv <- I, T1 <- N, T2 <- C
+            const CV cc = c->centroid();
             int i = 0;
-            for (FI f = c.facebegin(); f != c.faceend(); ++f, ++i) {
+            for (FI f = c->facebegin(); f != c->faceend(); ++f, ++i) {
                 Binv(i,i) = Scalar(1.0);
-                FA(i,i)   = f.area();
+                fa(i,i)   = f->area();
 
-                FV fc = f.centroid();  fc -= cc;  fc *= FA(i,i);
-                FV fn = f.normal  ();             fn *= FA(i,i);
+                FV fc = f->centroid();  fc -= cc;  fc *= fa(i,i);
+                FV fn = f->normal  ();             fn *= fa(i,i);
 
-                for (int j = 0; j < cc.size(); ++j) {
+                for (int j = 0; j < dim; ++j) {
                     T1(i,j) = fn[j];
                     T2(i,j) = fc[j];
                 }
             }
+            ASSERT(i == nf);
 
             // T2 <- orth(T2)
-            if (orthogonalizeColumns(T2, tau, work) != 0)
-            {
+            if (orthogonalizeColumns(T2, tau_, work_) != 0) {
                 ASSERT (false);
             }
 
             // Binv <- Binv - T2*T2' == I - Q*Q'
-            symmetricUpdate(-1, T2, 1, Binv);
+            symmetricUpdate(Scalar(-1.0), T2, Scalar(1.0), Binv);
 
             // Binv <- diag(A) * Binv * diag(A)
-            symmetricUpdate(FA, Binv);
+            symmetricUpdate(fa, Binv);
 
             // T2 <- N*K -- Assumes K (i.e., perm) is stored in C order
             // (i.e., transposed from the point of view of a FortranMatrix<T>).
             //
-            FortranMatrix<Scalar,false> K(nd, nd, &perm[0]);
-            MxM<Scalar,false,true>(Scalar(1.0), T1, K, Scalar(0.0), T2);
+            ASSERT(perm.size() == dim*dim);
+            FortranMatrix<Scalar,false> Kt(dim, dim, &perm[0]);
+            matMulAdd<Scalar,false,true>(Scalar(1.0), T1, Kt, Scalar(0.0), T2);
 
             // Binv <- (T2*N' + Binv) / vol(c)
             //      == (N*K*N' + t*(diag(A) * (I - Q*Q') * diag(A))) / vol(c)
@@ -116,10 +127,10 @@ namespace Dune {
             // where t = 6/d * TRACE(K) (== 2*TRACE(K) for 3D).
             //
             Scalar t = 0.0;
-            for (int j = 0; j < nd; ++j) t += K(j,j);
+            for (int j = 0; j < dim; ++j) t += K(j,j);
 
-            MxM<Scalar,false,true>(Scalar(1.0)     /       c.volume() , T2, T1,
-                                   Scalar(6.0) * t / (nd * c.volume()), Binv  );
+            matMulAdd<Scalar,false,true>(Scalar(1.0)     /        c->volume() , T2, T1,
+                                         Scalar(6.0) * t / (dim * c->volume()), Binv  );
         }
 
     private:
