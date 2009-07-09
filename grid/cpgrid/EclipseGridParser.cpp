6a52120a1bdf4884c43c50323edbc75fba7c8fb1
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2009-07-09 13:04:11 +0000

    As a (slight) performance improvement, reserve() the appropriate amount   of result space before constructing the list of known keywords (i.e.,   field names).

--- grid/cpgrid/EclipseGridParser.cpp
+++ grid/cpgrid/EclipseGridParser.cpp
@@ -6,30 +6,32 @@
 //
 // Author: Atgeirr F Rasmussen <atgeirr@sintef.no>
 //
-// $Date$
+// $Date:$
+//
+// $Revision:$
 //
 // Revision: $Id: EclipseGridParser.C,v 1.4 2008/08/18 14:16:14 atgeirr Exp $
 //
 //===========================================================================
 
 /*
-Copyright 2009 SINTEF ICT, Applied Mathematics.
-Copyright 2009 Statoil ASA.
+  Copyright 2009 SINTEF ICT, Applied Mathematics.
+  Copyright 2009 Statoil ASA.
 
-This file is part of The Open Reservoir Simulator Project (OpenRS).
+  This file is part of The Open Reservoir Simulator Project (OpenRS).
 
-OpenRS is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  OpenRS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-OpenRS is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  OpenRS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
@@ -331,6 +333,8 @@ vector<string> EclipseGridParser::fieldNames() const
 //---------------------------------------------------------------------------
 {
     vector<string> names;
+    names.reserve(integer_field_map_.size() +
+                  floating_field_map_.size());
     {
 	map<string, vector<int> >::const_iterator it = integer_field_map_.begin();
 	for (; it != integer_field_map_.end(); ++it) {
