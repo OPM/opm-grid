360d306f06b0251ce41e7c971fa166d9db3ebb56
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2009-09-04 15:05:14 +0000

    Don't assume that 'patch-dune.sh' is located in dune-cornerpoint.   Following 'make dist' to create a tar ball, the directory will be   named something like 'dune-cornerpoint-0.2'.

--- patch-dune.sh
+++ patch-dune.sh
@@ -1,5 +1,8 @@
 #!/bin/bash --norc
-for src in $(find dune-cornerpoint/changed_from_dune/ -type f | grep -v .svn); do
+
+d=${BASH_SOURCE%/*}
+
+for src in $(find ${d}/changed_from_dune/ -type f | grep -v .svn); do
 	f="${src##*dune/}"
 	dest="dune-${f%%/*}/${f}"
 	cmd="cp ${src} ${dest}"
