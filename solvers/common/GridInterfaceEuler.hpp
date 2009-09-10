1671df86e9835d231e5763c55810fe044f26a3f7
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2009-09-10 08:10:22 +0000

    Now added code that builds the face map, if an argument (defaulting to false) to the constructor/init method is true.

--- solvers/common/GridInterfaceEuler.hpp
+++ solvers/common/GridInterfaceEuler.hpp
@@ -43,8 +43,10 @@
 #include <boost/scoped_ptr.hpp>
 
 #include <dune/common/fvector.hh>
+#include <dune/common/SparseTable.hpp>
 #include <dune/grid/common/referenceelements.hh>
 #include <dune/grid/common/mcmgmapper.hh>
+
 
 namespace Dune
 {
@@ -348,17 +350,23 @@ namespace Dune
 	enum { Dimension = DuneGrid::dimension };
 
 	GridInterfaceEuler()
-	    : pgrid_(0)
+	    : pgrid_(0), num_faces_(0)
 	{
 	}
-	explicit GridInterfaceEuler(const DuneGrid& grid)
-	    : pgrid_(&grid), pmapper_(new Mapper(grid))
+	explicit GridInterfaceEuler(const DuneGrid& grid, bool build_facemap = false)
+	    : pgrid_(&grid), pmapper_(new Mapper(grid)), num_faces_(0)
 	{
+	    if (build_facemap) {
+		buildFaceIndices();
+	    }
 	}
-	void init(const DuneGrid& grid)
+	void init(const DuneGrid& grid, bool build_facemap = false)
 	{
 	    pgrid_ = &grid;
             pmapper_.reset(new Mapper(grid));
+	    if (build_facemap) {
+		buildFaceIndices();
+	    }
 	}
 	CellIterator cellbegin() const
 	{
@@ -372,19 +380,147 @@ namespace Dune
         {
             return grid().size(0);
         }
+        int numberOfFaces() const
+        {
+	    ASSERT(num_faces_ != 0);
+            return num_faces_;
+        }
 	const DuneGrid& grid() const
 	{
 	    ASSERT(pgrid_);
 	    return *pgrid_;
 	}
+
+	// The following are primarily helpers for the implementation,
+	// perhaps they should be private?
 	const Mapper& mapper() const
 	{
 	    ASSERT (pmapper_);
 	    return *pmapper_;
 	}
+	Index faceIndex(int cell_index, int local_face_index) const
+	{
+	    ASSERT(num_faces_ != 0);
+	    return face_indices_[cell_index][local_face_index];
+	}
     private:
 	const DuneGrid* pgrid_;
         boost::scoped_ptr<Mapper> pmapper_;
+	int num_faces_;
+	SparseTable<int> face_indices_;
+
+	void buildFaceIndices()
+	{
+#ifdef VERBOSE
+	    std::cout << "Building unique face indices... " << std::flush;
+#endif
+            typedef CellIterator CI;
+            typedef typename CI::FaceIterator FI;
+	    // First, we find the number of faces
+	    // for each cell.
+	    std::vector<int> num_faces(numberOfCells());
+	    for (CI c = cellbegin(); c != cellend(); ++c) {
+		int face_count = 0;
+		for (FI f = c->facebegin(); f != c->faceend(); ++f) {
+		    ++face_count;
+		}
+		num_faces[c->index()] = face_count;
+	    }
+	    std::vector<int> cumul_num_faces(numberOfCells() + 1);
+	    cumul_num_faces[0] = 0;
+	    std::partial_sum(num_faces.begin(), num_faces.end(), cumul_num_faces.begin() + 1);
+
+	    // Then, we build the actual cell to face mapping in two passes.
+	    // [code mostly lifted from IncompFlowSolverHybrid::enumerateGridDof()]
+            const int nc = numberOfCells();
+            std::vector<int> fpos           ;   fpos  .reserve(nc + 1);
+            std::vector<int> num_cf         ;   num_cf.reserve(nc);
+            std::vector<int> faces          ;
+
+            // Allocate cell structures.
+            std::vector<int> cell(nc, -1);
+
+            // First pass: enumerate internal faces.
+            int cellno = 0; fpos.push_back(0);
+            int tot_ncf = 0, tot_ncf2 = 0, max_ncf = 0;
+            for (CI c = cellbegin(); c != cellend(); ++c, ++cellno) {
+                const int c0 = c->index();
+                ASSERT((0 <= c0) && (c0 < nc) && (cell[c0] == -1));
+                cell[c0] = cellno;
+                num_cf.push_back(0);
+                int& ncf = num_cf.back();
+                for (FI f = c->facebegin(); f != c-> faceend(); ++f) {
+                    if (!f->boundary()) {
+                        const int c1 = f->neighbourCellIndex();
+                        ASSERT((0 <= c1) && (c1 < nc) && (c1 != c0));
+
+                        if (cell[c1] == -1) {
+                            // Previously undiscovered internal face.
+                            faces.push_back(c1);
+                        }
+                    }
+                    ++ncf;
+                }
+                fpos.push_back(int(faces.size()));
+                max_ncf  = std::max(max_ncf, ncf);
+                tot_ncf  += ncf;
+                tot_ncf2 += ncf * ncf;
+            }
+            ASSERT(cellno == nc);
+
+            // Avoid (most) allocation(s) inside 'c' loop.
+            std::vector<int>    l2g;
+	    l2g.reserve(max_ncf);
+	    std::vector<double> cfdata(tot_ncf);
+            int total_num_faces = int(faces.size());
+
+            // Second pass: build cell-to-face mapping, including boundary.
+            typedef std::vector<int>::iterator VII;
+            for (CI c = cellbegin(); c != cellend(); ++c) {
+                const int c0 = c->index();
+                ASSERT ((0 <=      c0 ) && (     c0  < nc) &&
+                        (0 <= cell[c0]) && (cell[c0] < nc));
+                const int ncf = num_cf[cell[c0]];
+                l2g.resize(ncf, 0);
+                for (FI f = c->facebegin(); f != c->faceend(); ++f) {
+                    if (f->boundary()) {
+                        // External, not counted before.  Add new face...
+                        l2g[f->localIndex()] = total_num_faces++;
+                    } else {
+                        // Internal face.  Need to determine during
+                        // traversal of which cell we discovered this
+                        // face first, and extract the face number
+                        // from the 'faces' table range of that cell.
+
+                        // Note: std::find() below is potentially
+                        // *VERY* expensive (e.g., large number of
+                        // seeks in moderately sized data in case of
+                        // faulted cells).
+                        const int c1 = f->neighbourCellIndex();
+                        ASSERT ((0 <=      c1 ) && (     c1  < nc) &&
+                                (0 <= cell[c1]) && (cell[c1] < nc));
+
+                        int t = c0, seek = c1;
+                        if (cell[seek] < cell[t])
+                            std::swap(t, seek);
+                        int s = fpos[cell[t]], e = fpos[cell[t] + 1];
+                        VII p = std::find(faces.begin() + s, faces.begin() + e, seek);
+                        ASSERT(p != faces.begin() + e);
+                        l2g[f->localIndex()] = p - faces.begin();
+                    }
+                }
+		ASSERT(int(l2g.size()) == num_faces[c0]);
+		std::copy(l2g.begin(), l2g.end(), cfdata.begin() + cumul_num_faces[c0]);
+            }
+	    num_faces_ = total_num_faces;
+            face_indices_.assign(cfdata.begin(), cfdata.end(),
+				 num_faces.begin(), num_faces.end());
+
+#ifdef VERBOSE
+	    std::cout << "done." << std::endl;
+#endif
+	}
+
     };
 
 
