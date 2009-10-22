d045ea3ed4ee98e0edbbbbd7bd91c031754f736b
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2009-10-22 16:21:43 +0000

    Added unit tests for NonuniformTableLinear

---
+++ solvers/common/test/nonuniformtablelinear_test.cpp
@@ -0,0 +1,95 @@
+//===========================================================================
+//
+// File: nonuniformtablelinear_test.cpp
+//
+// Created: Thu Oct 22 15:28:31 2009
+//
+// Author(s): Atgeirr F Rasmussen <atgeirr@sintef.no>
+//            Bård Skaflestad     <bard.skaflestad@sintef.no>
+//
+// $Date$
+//
+// $Revision$
+//
+//===========================================================================
+
+/*
+  Copyright 2009 SINTEF ICT, Applied Mathematics.
+  Copyright 2009 Statoil ASA.
+
+  This file is part of The Open Reservoir Simulator Project (OpenRS).
+
+  OpenRS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OpenRS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#define BOOST_TEST_DYN_LINK
+#define NVERBOSE // to suppress our messages when throwing
+
+
+#define BOOST_TEST_MODULE NonuniformTableLinearTests
+#include <boost/test/unit_test.hpp>
+
+#include "../NonuniformTableLinear.hpp"
+
+
+BOOST_AUTO_TEST_CASE(utility_functions)
+{
+    // Test isNondecreasing().
+    using Dune::utils::isNondecreasing;
+    double xva1[] = { -1.0, 2.0, 2.2, 3.0, 5.0 };
+    const int numvals1 = sizeof(xva1)/sizeof(xva1[0]);
+    BOOST_CHECK(isNondecreasing(xva1, xva1 + numvals1));
+    double xva2[] = { -1.0, 2.0, 2.0, 2.0, 5.0 };
+    const int numvals2 = sizeof(xva2)/sizeof(xva2[0]);
+    BOOST_CHECK(isNondecreasing(xva2, xva2 + numvals2));
+    double xva3[] = { -1.0, 2.0, 1.9, 3.0, 5.0 };
+    const int numvals3 = sizeof(xva3)/sizeof(xva3[0]);
+    BOOST_CHECK(!isNondecreasing(xva3, xva3 + numvals3));
+}
+
+BOOST_AUTO_TEST_CASE(table_operations)
+{
+    // Make a simple table.
+    double xva[] = { -1.0, 2.0, 2.2, 3.0, 5.0 };
+    const int numvals = sizeof(xva)/sizeof(xva[0]);
+    std::vector<double> xv(xva, xva + numvals);
+    double yva[numvals] = { 1.0, 2.0, 3.0, 4.0, 2.0 };
+    std::vector<double> yv(yva, yva + numvals);
+    Dune::utils::NonuniformTableLinear<double> t1(xv, yv);
+
+    // Check some evaluations.
+    for (int i = 0; i < numvals; ++i) {
+	BOOST_CHECK_EQUAL(t1(xv[i]), yv[i]);
+    }
+    BOOST_CHECK_EQUAL(t1(2.6), 3.5);
+    BOOST_CHECK_EQUAL(t1(4.0), 3.0);
+    BOOST_CHECK_EQUAL(t1.derivative(4.0), -1.0);
+    // Until we implement anything but the ClosestValue end policy, we only test that.
+    BOOST_CHECK_EQUAL(t1(xv[0] - 1.0), yv[0]);
+    BOOST_CHECK_EQUAL(t1(xv.back() + 1.0), yv.back());
+
+    // Domains.
+    BOOST_CHECK_EQUAL(t1.domain().first, xv[0]);
+    BOOST_CHECK_EQUAL(t1.domain().second, xv.back());
+    std::pair<double, double> new_domain(-100.0, 20.0);
+    t1.rescaleDomain(new_domain);
+    BOOST_CHECK_EQUAL(t1.domain().first, new_domain.first);
+    BOOST_CHECK_EQUAL(t1.domain().second, new_domain.second);
+    for (int i = 0; i < numvals; ++i) {
+	BOOST_CHECK_EQUAL(t1((xv[i] + 1.0)*20.0 - 100.0), yv[i]);
+    }
+    BOOST_CHECK_EQUAL(t1(0.0), 3.0);
+    BOOST_CHECK(std::fabs(t1.derivative(0.0)  + 1.0/20.0) < 1e-11);
+}
