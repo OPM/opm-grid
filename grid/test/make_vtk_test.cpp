dbf93f85c2a07777b8666b850378999821b2ed2a
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2009-12-23 13:06:08 +0000

    No longer assume input filename ends in .grdecl.

--- grid/test/make_vtk_test.cpp
+++ grid/test/make_vtk_test.cpp
@@ -64,6 +64,6 @@ int main(int argc, char** argv)
     vtkwriter.addCellData(perm_yy, "perm_yy");
     vtkwriter.addCellData(perm_zz, "perm_zz");
     std::string fname = param.get<std::string>("filename");
-    vtkwriter.write(fname.substr(0, fname.size() - 7) + "-cells", VTKOptions::ascii);
+    vtkwriter.write(fname.substr(0, fname.find_last_of('.')) + "-cells", VTKOptions::ascii);
 }
 
