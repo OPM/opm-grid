500e0fb4e00330fa0a3fdfbc02e89c4717715706
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2010-03-17 17:29:39 +0000

    Turn into no-op as we now run with stock sources.

--- patch-dune.sh
+++ patch-dune.sh
@@ -1,11 +1,3 @@
 #!/bin/bash --norc
 
-d=${BASH_SOURCE%/*}
-
-for src in $(find ${d}/changed_from_dune/ -type f | grep -v .svn); do
-	f="${src##*dune/}"
-	dest="dune-${f%%/*}/dune/${f}"
-	cmd="cp ${src} ${dest}"
-	echo "${cmd}"
-	${cmd}
-done
+true
