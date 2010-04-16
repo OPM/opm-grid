2e2355bffb7739ef395b33f9e0a6dbd55fa8f024
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2010-04-16 07:00:53 +0000

    Added a new fluid class with fixed mobilities.

---
+++ solvers/common/ReservoirPropertyFixedMobility.hpp
@@ -0,0 +1,73 @@
+//===========================================================================
+//
+// File: ReservoirPropertyFixedMobility.hpp
+//
+// Created: Thu Apr 15 14:50:22 2010
+//
+// Author(s): Atgeirr F Rasmussen <atgeirr@sintef.no>
+//            Jostein R Natvig    <jostein.r.natvig@sintef.no>
+//
+// $Date$
+//
+// $Revision$
+//
+//===========================================================================
+
+/*
+  Copyright 2010 SINTEF ICT, Applied Mathematics.
+  Copyright 2010 Statoil ASA.
+
+  This file is part of The Open Reservoir Simulator Project (OpenRS).
+
+  OpenRS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OpenRS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OpenRS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPENRS_RESERVOIRPROPERTYFIXEDMOBILITY_HEADER
+#define OPENRS_RESERVOIRPROPERTYFIXEDMOBILITY_HEADER
+
+
+#include <vector>
+
+
+namespace Dune {
+
+    class ReservoirPropertyFixedMobility {
+    public:
+        ReservoirPropertyFixedMobility(const std::vector<double>& mobs)
+            : mobs_(mobs)
+        {
+        }
+
+        enum { NumberOfPhases = 2 };
+
+        template<class Vector>
+        void phaseMobilities(int cell_index, double /*saturation*/, Vector& mobility) const
+        {
+            mobility[0] = mobs_[cell_index];
+            mobility[1] = 0.0;
+        }
+
+        template<class Vector>
+        void phaseDensities(int /*cell_index*/, Vector& rho) const
+        {
+            rho[0] = 1.0;
+            rho[1] = 0.0;
+        }
+    private:
+        const std::vector<double>& mobs_;
+    };
+}
+
+
+#endif // OPENRS_RESERVOIRPROPERTYFIXEDMOBILITY_HEADER
