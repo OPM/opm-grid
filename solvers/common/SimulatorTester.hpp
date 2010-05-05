1e06c305b96c761df34fdace5142779148374c26
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2010-05-05 13:31:09 +0000

    Added more output to vtk, in line with that from the upscaling code.

--- solvers/common/SimulatorTester.hpp
+++ solvers/common/SimulatorTester.hpp
@@ -58,7 +58,7 @@ namespace Dune
 	void run()
 	{
 	    // Initial saturation.
-	    std::vector<double> sat(Super::init_saturation_);
+	    std::vector<double> saturation(Super::init_saturation_);
 	    // Gravity.
 	    // FieldVector<double, 3> gravity(0.0);
 	    // gravity[2] = -Dune::unit::gravity;
@@ -72,27 +72,39 @@ namespace Dune
 		std::cout << "\n\n================    Simulation step number " << i
                           << "    ===============" << std::endl;
 		// Flow.
-		Super::flow_solver_.solve(Super::res_prop_, sat, Super::bcond_, Super::injection_rates_psolver_,
+		Super::flow_solver_.solve(Super::res_prop_, saturation, Super::bcond_, Super::injection_rates_psolver_,
                                           Super::residual_tolerance_, Super::linsolver_verbosity_, Super::linsolver_type_);
 // 		if (i == 0) {
 // 		    flow_solver_.printSystem("linsys_dump_mimetic");
 // 		}
 		// Transport.
-		Super::transport_solver_.transportSolve(sat, Super::stepsize_, Super::gravity_,
+		Super::transport_solver_.transportSolve(saturation, Super::stepsize_, Super::gravity_,
 							Super::flow_solver_.getSolution(),
 							Super::injection_rates_);
 		// Output.
-		std::vector<typename Super::Vector> cell_velocity;
-		estimateCellVelocity(cell_velocity, Super::ginterf_, Super::flow_solver_.getSolution());
+                typedef typename Super::GridInterface::Vector Vec;
+                std::vector<Vec> cell_velocity;
+                estimateCellVelocity(cell_velocity, this->ginterf_, this->flow_solver_.getSolution());
+                Dune::array<std::vector<Vec>, 2> phase_velocities;
+                computePhaseVelocities(phase_velocities[0], phase_velocities[1], this->res_prop_, saturation, cell_velocity);
                 // Dune's vtk writer wants multi-component data to be flattened.
                 std::vector<double> cell_velocity_flat(&*cell_velocity.front().begin(),
                                                        &*cell_velocity.back().end());
-		std::vector<double> cell_pressure;
-		getCellPressure(cell_pressure, Super::ginterf_, Super::flow_solver_.getSolution());
+                std::vector<double> water_velocity_flat(&*phase_velocities[0].front().begin(),
+                                                       &*phase_velocities[0].back().end());
+                std::vector<double> oil_velocity_flat(&*phase_velocities[1].front().begin(),
+                                                       &*phase_velocities[1].back().end());
+                std::vector<double> cell_pressure;
+                getCellPressure(cell_pressure, this->ginterf_, this->flow_solver_.getSolution());
+                std::vector<double> cap_pressure;
+                computeCapPressure(cap_pressure, this->res_prop_, saturation);
 		Dune::VTKWriter<typename Super::GridType::LeafGridView> vtkwriter(Super::grid_.leafView());
-		vtkwriter.addCellData(cell_velocity_flat, "velocity", 3);
-		vtkwriter.addCellData(sat, "saturation");
-		vtkwriter.addCellData(cell_pressure, "pressure");
+                vtkwriter.addCellData(cell_velocity_flat, "velocity", Vec::dimension);
+                vtkwriter.addCellData(water_velocity_flat, "phase velocity [water]", Vec::dimension);
+                vtkwriter.addCellData(oil_velocity_flat, "phase velocity [oil]", Vec::dimension);
+                vtkwriter.addCellData(saturation, "saturation");
+                vtkwriter.addCellData(cell_pressure, "pressure");
+                vtkwriter.addCellData(cap_pressure, "capillary pressure");
 		vtkwriter.write("testsolution-" + boost::lexical_cast<std::string>(i),
                                 Dune::VTKOptions::ascii);
 	    }
