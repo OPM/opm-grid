OPM/opm-core

Move old files into the attic
The attic are the place where files which we don't use right now, but
which we acknowledge some amount of valuable time has been spent on and
which may be usable some day in the future, so we don't have the
conscience to right out delete them.

These files are not expected to compile, and certainly not to run and
produce sensible results, anymore. However, with some work they can
possibly be converted into proper unit tests or examples.

CMakeLists_files.cmake
@@ -22,6 +22,9 @@
 #	                      files can of course include other files than these;
 #	                      you should only add to this list if the *user* of
 #	                      the library needs it.
+#
+# ATTIC_FILES           Unmaintained files. This for the projects developers
+#                       only. Don't expect these files to build.
 
 # originally generated with the command:
 # find opm -name '*.c*' -printf '\t%p\n' | sort
@@ -176,6 +179,23 @@ list (APPEND EXAMPLE_SOURCE_FILES
 	tutorials/tutorial2.cpp
 	tutorials/tutorial3.cpp
 	tutorials/tutorial4.cpp
+	)
+
+# originally generated with the command:
+# find attic -name '*.c*' -printf '\t%p\n' | sort
+list (APPEND ATTIC_FILES
+	attic/bo_resprop_test.cpp
+	attic/pvt_test.cpp
+	attic/relperm_test.cpp
+	attic/test_agmg.cpp
+	attic/test_cfs_tpfa.c
+	attic/test_ert.cpp
+	attic/test_jacsys.cpp
+	attic/test_lapack.cpp
+	attic/test_read_grid.c
+	attic/test_readpolymer.cpp
+	attic/test_read_vag.cpp
+	attic/test_writeVtkData.cpp
 	)
 
 # programs listed here will not only be compiled, but also marked for

OPM/opm-grid

CMakeLists_files.cmake
@@ -23,8 +23,8 @@
 #	                      you should only add to this list if the *user* of
 #	                      the library needs it.
 #
-# NOT_UNIT_TEST_SOURCE_FILES List of programms that will be build during 
-#                            make non-unit-tests
+# ATTIC_FILES           Unmaintained files. This for the projects developers
+#                       only. Don't expect these files to build.
 
 # originally generated with the command:
 # find dune -name '*.c*' -printf '\t%p\n' | sort
@@ -39,14 +39,14 @@ list (APPEND MAIN_SOURCE_FILES
 
 # originally generated with the command:
 # find tests/not-unit/ -name \*.cpp -o \*.cc
-list (APPEND NOT_UNIT_TEST_SOURCE_FILES
-  tests/not-unit/partition_test.cpp
-#  tests/not-unit/dumux_test.cpp
-  tests/not-unit/mapper_test.cpp
-  tests/not-unit/cpgrid/buildcpgrid_test.cpp
-#  tests/not-unit/cpgrid_test.cpp
-  tests/not-unit/max_zdist_test.cpp
-  tests/not-unit/check_grid_normals.cpp
+list (APPEND ATTIC_FILES
+	attic/partition_test.cpp
+#	attic/dumux_test.cpp
+	attic/mapper_test.cpp
+	attic/buildcpgrid_test.cpp
+#	attic/cpgrid_test.cpp
+	attic/max_zdist_test.cpp
+	attic/check_grid_normals.cpp
   )
 
 # originally generated with the command: