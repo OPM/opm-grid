34a08dcc9bf943ad12e15fd1e62c8cae8ff814ee
Markus Blatt <markus@dr-blatt.de>
2013-12-05 14:38:39 +0100

    Fixes method subid on the indexset.
    It did not compile before this patch as id was called with an entity
    pointer and not an entity.

--- dune/grid/cpgrid/Indexsets.hpp
+++ dune/grid/cpgrid/Indexsets.hpp
@@ -259,10 +259,10 @@ namespace Dune
 	    IdType subId(const cpgrid::Entity<0>& e, int i, int cc) const
 	    {
 		switch (cc) {
-		case 0: return id(e.subEntity<0>(i));
-		case 1: return id(e.subEntity<1>(i));
-		case 2: return id(e.subEntity<2>(i));
-		case 3: return id(e.subEntity<3>(i));
+		case 0: return id(*e.subEntity<0>(i));
+		case 1: return id(*e.subEntity<1>(i));
+		case 2: return id(*e.subEntity<2>(i));
+		case 3: return id(*e.subEntity<3>(i));
 		default: OPM_THROW(std::runtime_error, "Cannot get subId of codimension " << cc);
 		}
 		return -1;
