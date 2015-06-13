36e0acbd5f60ca2ba9d1274c0573a53d273f4d13
Andreas Lauser <and@poware.org>
2015-06-13 23:57:52 +0200

    polyhedralgrid: instantiate a grid object in the unit test

--- tests/polyhedralgrid_test.cc
+++ tests/polyhedralgrid_test.cc
@@ -2,6 +2,29 @@
 
 #include <dune/grid/polyhedralgrid.hh>
 
+#include <opm/parser/eclipse/Parser/Parser.hh>
+#include <opm/parser/eclipse/Deck/Deck.hh>
+
+#include <iostream>
+
+const char *deckString =
+    "RUNSPEC\n"
+    "DIMENS\n"
+    "10 10 10 /\n"
+    "GRID\n"
+    "DXV\n"
+    "10*1 /\n"
+    "DYV\n"
+    "10*1 /\n"
+    "DZV\n"
+    "10*1 /\n";
+
 int main()
 {
+    Opm::Parser parser;
+    const auto deck = parser.parseString(deckString);
+
+    PolyhedralGrid<dim, dimworld> grid(deck);
+
+    return 0;
 }
