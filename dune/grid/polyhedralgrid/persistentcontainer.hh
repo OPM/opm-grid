e1a1feb8365aa850ab9ea73a901acbf9c7ed0338
Robert Kloefkorn <robert.kloefkorn@iris.no>
2015-06-13 23:18:37 +0200

    adjust to polyhedral grid.

--- dune/grid/polyhedralgrid/persistentcontainer.hh
+++ dune/grid/polyhedralgrid/persistentcontainer.hh
@@ -1,31 +1,59 @@
 #ifndef DUNE_POLYHEDRALGRID_PERSISTENTCONTAINER_HH
 #define DUNE_POLYHEDRALGRID_PERSISTENTCONTAINER_HH
 
-#include <dune/grid/utility/persistentcontainerwrapper.hh>
+#include <dune/common/version.hh>
 
-#include <dune/grid/polyhedralgrid/declaration.hh>
+#include <dune/grid/utility/persistentcontainer.hh>
+#include <dune/grid/polyhedralgrid/grid.hh>
+
+#if DUNE_VERSION_NEWER(DUNE_GRID, 2, 3)
+#include <dune/grid/utility/persistentcontainervector.hh>
+#endif
 
 namespace Dune
 {
-
-  // PersistentContainer for PolyhedralGrid
-  // ------------------------------
-
-  template< int dim, int dimworld, class T >
-  class PersistentContainer< PolyhedralGrid< dim, dimworld >, T >
-  : public PersistentContainerWrapper< PolyhedralGrid< dim, dimworld >, T >
+  // PersistentContainer for CpGrid
+  // -------------------------------
+#if DUNE_VERSION_NEWER(DUNE_GRID, 2, 3)
+  template< int dim, int dimworld, class Data >
+  class PersistentContainer< PolyhedralGrid< dim, dimworld >, Data >
+  : public PersistentContainerVector< PolyhedralGrid< dim, dimworld >,
+                                      typename PolyhedralGrid< dim, dimworld >::Traits::LeafIndexSet,
+                                      std::vector<Data> >
+#else
+  template< int dim, int dimworld, class Data, class Allocator >
+  class PersistentContainer< PolyhedralGrid< dim, dimworld >, Data, Allocator >
+  : public PersistentContainerVector< PolyhedralGrid< dim, dimworld >,
+                                      typename PolyhedralGrid< dim, dimworld >::Traits::LeafIndexSet,
+                                      std::vector<Data,Allocator> >
+#endif
   {
-    typedef PersistentContainerWrapper< PolyhedralGrid< dim, dimworld >, T > Base;
+  public:
+    typedef CpGrid  GridType;
+#if DUNE_VERSION_NEWER(DUNE_GRID, 2, 3)
+    typedef typename std::vector<Data>::allocator_type Allocator;
+#endif
+
+  private:
+#if DUNE_VERSION_NEWER(DUNE_GRID, 2, 3)
+    typedef PersistentContainerVector< GridType, typename GridType::Traits::LeafIndexSet, std::vector<Data> > BaseType;
+#else
+    typedef PersistentContainerVector< GridType, typename GridType::Traits::LeafIndexSet, std::vector<Data,Allocator> > BaseType;
+#endif
 
   public:
-    typedef typename Base::Grid Grid;
-    typedef typename Base::Value Value;
-
-    PersistentContainer ( const Grid &grid, int codim, const Value &value = Value() )
-    : Base( grid, codim, value )
+    //! Constructor filling the container with values using the default constructor
+    //! Depending on the implementation this could be achieved without allocating memory
+#if DUNE_VERSION_NEWER(DUNE_GRID, 2, 3)
+    PersistentContainer ( const GridType &grid, const int codim, const Data& data = Data(), const Allocator &allocator = Allocator() )
+    : BaseType( grid.leafIndexSet(), codim, data, allocator )
+#else
+    PersistentContainer ( const GridType &grid, const int codim, const Allocator &allocator = Allocator() )
+    : BaseType( grid, codim, grid.leafIndexSet(), 1.1, allocator )
+#endif
     {}
   };
 
-} // namespace Dune
+} // end namespace Dune
 
-#endif // #ifndef DUNE_POLYHEDRALGRID_PERSISTENTCONTAINER_HH
+#endif
