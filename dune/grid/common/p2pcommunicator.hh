5f71d29a15ef49106186df2f9610f3187ab470f7
Robert Kloefkorn <robert.kloefkorn@iris.no>
2015-09-15 11:29:58 +0200

    Point2PointCommunicator: adhere the MPI standard.

--- dune/grid/common/p2pcommunicator.hh
+++ dune/grid/common/p2pcommunicator.hh
@@ -232,8 +232,7 @@ public:
       const int retTag = tag;
       tag += increment ;
       // the MPI standard guaratees only up to 2^15-1
-      // this needs to be revised for the all-to-all communication
-      if( tag < 0 ) // >= 32767 )
+      if( tag >= 32767 )
       {
         // reset tag to initial value
         tag = messagetag + 2 ;
