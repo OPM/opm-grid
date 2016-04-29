2edc1a00232886a4282598af9756384c68daa142
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-04-29 22:52:43 +0200

    added: downstream build support in the jenkins trigger
    use 'jenkins build this with downstreams please'.
    
    this will build all downstreams against the PR, and
    execute their tests. PR's for upstream and downstream modules
    can be specified.
    
    the aggregated test results are attached to the job result on jenkins.

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,11 +2,18 @@
 
 source `dirname $0`/build-opm-grid.sh
 
+declare -a upstreams
+upstreams=(opm-parser
+           opm-material
+           opm-core)
+
+declare -A upstreamRev
+upstreamRev[opm-parser]=master
+upstreamRev[opm-material]=master
+upstreamRev[opm-core]=master
+
 ERT_REVISION=master
 OPM_COMMON_REVISION=master
-OPM_PARSER_REVISION=master
-OPM_MATERIAL_REVISION=master
-OPM_CORE_REVISION=master
 
 build_opm_grid
 test $? -eq 0 || exit 1
