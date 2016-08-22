ab8ce0b1ae6d617fc7c5319841dacb255f3971c2
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-08-22 13:36:32 +0200

    simplify jenkins build scripts after ert reorganization

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,18 +3,19 @@
 source `dirname $0`/build-opm-grid.sh
 
 declare -a upstreams
-upstreams=(opm-parser
+upstreams=(ert
+           opm-parser
            opm-output
            opm-material
            opm-core)
 
 declare -A upstreamRev
+upstreamRev[ert]=master
 upstreamRev[opm-parser]=master
 upstreamRev[opm-output]=master
 upstreamRev[opm-material]=master
 upstreamRev[opm-core]=master
 
-ERT_REVISION=master
 OPM_COMMON_REVISION=master
 
 build_opm_grid
