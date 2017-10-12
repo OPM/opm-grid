fe8728e998b726b65978cf5626a1f27d24dd6937
Andreas Lauser <and@poware.org>
2017-10-12 17:27:47 +0200

    replace #if HAVE_CONFIG_H by #ifdef HAVE_CONFIG_H
    it seems like most build systems pass a -DHAVE_CONFIG_H flag to the
    compiler which still causes `#if HAVE_CONFIG_H` to be false while it
    clearly is supposed to be triggered.
    
    That said, I do not really see a good reason why the inclusion of the
    `config.h` file should be guarded in the first place: the file is
    guaranteed to always available by proper build systems, and if it was
    not included the build either breaks at the linking stage or -- at the
    very least -- the runtime behavior of the resulting libraries will be
    very awkward.

--- attic/mapper_test.cpp
+++ attic/mapper_test.cpp
@@ -13,7 +13,7 @@
  *                                                                           *
  *   This program is distributed WITHOUT ANY WARRANTY.                       *
  *****************************************************************************/
-#if HAVE_CONFIG_H
+#ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
