bcbd5448a763bea3fa7c6bb60f0d1f6265a2b467
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-06-09 11:17:19 +0200

    GridHelpers: remove unused method faceTag

--- opm/grid/GridHelpers.cpp
+++ opm/grid/GridHelpers.cpp
@@ -296,12 +296,6 @@ double faceArea(const UnstructuredGrid& grid, int face_index)
     return grid.face_areas[face_index];
 }
 
-int faceTag(const UnstructuredGrid& grid,
-            boost::iterator_range<const int*>::const_iterator face)
-{
-    return grid.cell_facetag[face-cell2Faces(grid)[0].begin()];
-}
-
 SparseTableView cell2Faces(const UnstructuredGrid& grid)
 {
     return SparseTableView(grid.cell_faces, grid.cell_facepos, numCells(grid));
