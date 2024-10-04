eb7c565ae182424432d9f81715442786ff249cdb
Michal Tóth <michal.toth@opm-op.com>
2024-10-04 17:35:26 +0200

    Create graph representation of grid.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -126,6 +126,7 @@ if(Boost_VERSION_STRING VERSION_GREATER 1.53)
 	  tests/cpgrid/lookUpCellCentroid_cpgrid_test.cpp
 	  tests/cpgrid/lookupdataCpGrid_test.cpp
 	  tests/cpgrid/shifted_cart_test.cpp
+    tests/test_graphofgrid.cpp
   )
 endif()
 
