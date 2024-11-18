5d20631820efbcd904e4921174fab5b9722908f4
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2024-11-18 13:10:10 +0100

    Changes for more resonable partitioning: - option to not remove anything from partitioning graph - added possibility for setting overlap - changed export list: need testing in particular in parallel (if it is intended to work there)

--- opm/grid/CpGrid.hpp
+++ opm/grid/CpGrid.hpp
@@ -714,7 +714,8 @@ namespace Dune
         bool loadBalance(int overlapLayers=1,
                          int partitionMethod = Dune::PartitionMethod::zoltan,
                          double imbalanceTol = 1.1,
-                         int level =-1)
+                         int level =-1,
+                         bool addCornerCells = true)
         {
             using std::get;
             return get<0>(scatterGrid(/* edgeWeightMethod = */ defaultTransEdgeWgt,
@@ -723,7 +724,7 @@ namespace Dune
                                       /* possibleFutureConnections = */ {},
                                       /* serialPartitioning = */ false,
                                       /* transmissibilities = */ nullptr,
-                                      /* addCornerCells = */ true,
+                                      addCornerCells,
                                       overlapLayers,
                                       partitionMethod,
                                       imbalanceTol,
