b2c51914ee2bc97093417dabc412b0a0afdacfaa
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2024-12-17 10:10:10 +0100

    fixed compilation

--- opm/grid/cpgpreprocess/make_edge_conformal.hpp
+++ opm/grid/cpgpreprocess/make_edge_conformal.hpp
@@ -2,6 +2,13 @@
 #define OPENRS_MAKE_EDGE_CONFORMAL_HEADER
 
 #include "preprocess.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void make_edge_conformal(struct processed_grid* grdecl);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
