2d6d35bbbbb0a1205382619f40aaaf1cc2fa62f7
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2025-02-10 18:20:36 +0100

    Add Experimental Edge Conformal CPG Processing Mode
    Initially supported fully only for the 'UnstructuredGrid'
    (create_grid_cornerpoint() constructor, PolyhedralGrid wrapper),
    this commit introduces amended logic that aims to create edge
    conformal cell complexes.  Specifically, the faces of the
    'processed_grid' structure from 'process_grdecl()' contains *all*
    vertices along the pillars, not just the top and bottom vertices of
    the traditional corner-point description.  Moreover, the
    create_grid_cornerpoint() will insert additional vertices in the top
    and bottom *faces* when the edge conformal mode is activated.  This
    aspect is not yet available in the CpGrid wrapper, though the
    vertices along the pillars will be included in that case too.
    
    Client code may activate this mode by setting the 'edge_conformal'
    flag to 'true'.  The flag is a bool (i.e., _Bool) in both the C and
    the C++ code, but transmitted as an 'int' across the language
    boundary.
    
    At this time, the mode is intended to support geo-mechanical
    workflows and should typically not be enabled in production runs of
    regular reservoir simulations.
    
    While here, split some long lines and mark objects 'const' where
    possible.

--- tests/test_column_extract.cpp
+++ tests/test_column_extract.cpp
@@ -138,7 +138,7 @@ BOOST_AUTO_TEST_CASE(DisjointColumn)
     for (size_t i = 1; i <= (3 * 3 * 3); i++)
         actnum.push_back(i != 14); // ACTNUM 13*1 0 13* 1
     ep.resetACTNUM(actnum);
-    Opm::GridManager manager(ep);
+    Opm::GridManager manager(ep, /* edge_conformal = */ false);
 
     VVI columns;
     Opm::extractColumn(*manager.c_grid(), columns);
