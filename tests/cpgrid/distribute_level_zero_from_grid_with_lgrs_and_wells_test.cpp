e6d0e14dcf9da243a1f8b1dd66b2015bb7fd8e77
Antonella Ritorto <anto.ritorto@gmail.com>
2025-04-10 09:55:28 +0200

    Delete cases, add grid created with deck, clean up test

--- tests/cpgrid/distribute_level_zero_from_grid_with_lgrs_and_wells_test.cpp
+++ tests/cpgrid/distribute_level_zero_from_grid_with_lgrs_and_wells_test.cpp
@@ -30,22 +30,21 @@
 #define BOOST_TEST_MODULE DistributeGridWithLgrsAndWellsTest
 #define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
-#include <opm/input/eclipse/Deck/Deck.hpp>
-#include <opm/input/eclipse/Parser/Parser.hpp>
-#include <opm/input/eclipse/EclipseState/EclipseState.hpp>
-#include <opm/grid/CpGrid.hpp>
 
-#include <opm/grid/GraphOfGrid.hpp>
-#include <opm/grid/GraphOfGridWrappers.hpp>
+#include <opm/grid/CpGrid.hpp>
+#include <tests/cpgrid/LgrChecks.hpp>
+#include <opm/grid/common/WellConnections.hpp>
 
 #include <opm/grid/utility/OpmWellType.hpp>
 #include <opm/input/eclipse/Schedule/Well/Connection.hpp>
 #include <opm/input/eclipse/Schedule/Well/WellConnections.hpp>
 #include <opm/input/eclipse/Schedule/Well/Well.hpp>
 
-#include <algorithm>
+#include <memory>
+#include <set>
 #include <string>
-
+#include <unordered_map>
+#include <vector>
 
 namespace {
 // create Wells, we only use well name and cell locations
@@ -65,7 +64,7 @@ auto createWell (const std::string& name)
 };
 } // end anonymous namespace
 
-BOOST_AUTO_TEST_CASE(loadBalance_grid_with_lgrs_and_coarseCellWells)
+BOOST_AUTO_TEST_CASE(add_wells_and_loadBalance_level_zero_of_cartesian_cpgrid_with_lgrs)
 {
     // create a grid with lgrs and wells
     Dune::CpGrid grid;
@@ -80,135 +79,15 @@ BOOST_AUTO_TEST_CASE(loadBalance_grid_with_lgrs_and_coarseCellWells)
     //
     // k = 0   1   | k = 1  [3]  | k = 2   [5]   | k = 3   7   |
     //         0   |        [2]  |         [4]   |         6   |
-    //
 
-    // leaf grid view (LGR1 parent cell indices = {2,3,4,5}, LGR1 dimension 2x4x2)
-    //
-    // k = 0   1   | k = 1  [6 7 8 9]  | k = 2   [14 15 16 17]   | k = 3   19  |
-    //         0   |        [2 3 4 5]  |         [10 11 12 13]   |         18  |
-
-    // coarse cells leaf indices 0 and 1 -> well1
-    // (equivalent coarse cells in level zero with indices 0 and 1 -> well1)
-    auto wellCon = std::make_shared<Opm::WellConnections>();
-    wellCon->add(createConnection(0,0,0)); // {level 0, cell idx 0} -> ijk = {0,0,0}
-    wellCon->add(createConnection(0,1,0)); // {level 0, cell idx 1} -> ijk = {0,1,0}
-    std::vector<Dune::cpgrid::OpmWellType> wells;
-    wells.push_back(createWell("well1"));
-    wells[0].updateConnections(wellCon,true);
-
-
-    // coarse cells leaf indices 18 and 19 -> well2
-    // (equivalent coarse cells in level zero with indices 6 and 7 -> well2)
-    wellCon = std::make_shared<Opm::WellConnections>(); // reset
-    wellCon->add(createConnection(0,0,3)); // {level 0, cell idx 6} -> ijk = {0,0,3}
-    wellCon->add(createConnection(0,1,3)); // {level 0, cell idx 7} -> ijk = {0,1,3}
-
-    wells.push_back(createWell("well2"));
-    wells[1].updateConnections(wellCon,true);
-
-    if (grid.comm().size()>1) {
-
-        grid.loadBalance(&wells,
-                         /* possibleFutureConnections = */ {},
-                         /* transmissibilities = */ nullptr,
-                         /* overlapLayers = */ 1,
-                         /* partitionMethod = */ Dune::PartitionMethod::zoltanGoG,
-                         /* level = */ 0);
-
-    }
-}
-
-BOOST_AUTO_TEST_CASE(loadBalance_grid_with_lgrs_and_refinedCellWells)
-{
-    // create a grid with lgrs and wells
-    Dune::CpGrid grid;
-    grid.createCartesian(/* grid_dims = */ {1,2,4}, /* size = */ {1.,1.,1.});
-
-    grid.addLgrsUpdateLeafView( /* cells_per_dim_vec = */ {{2,2,1}},
-                                /* startIJK_vec = */ {{0,0,1}},
-                                /* endIJK_vec = */ {{1,2,3}},
-                                /* lgr_name_vec = */ {"LGR1"});
-
-    // level zero grid view
-    //
-    // k = 0   1   | k = 1  [3]  | k = 2   [5]   | k = 3   7   |
-    //         0   |        [2]  |         [4]   |         6   |
-    //
-
-    // leaf grid view (LGR1 parent cell indices = {2,3,4,5}, LGR1 dimension 2x4x2)
-    //
-    // k = 0   1   | k = 1  [6 7 8 9]  | k = 2   [14 15 16 17]   | k = 3   19  |
-    //         0   |        [2 3 4 5]  |         [10 11 12 13]   |         18  |
-
-    // refined cells leaf indices [2 3 4 5] and [10 11 12 13] -> well1
-    // (parent cells in level zero with indices 2 and 4 -> well1)
-    auto wellCon = std::make_shared<Opm::WellConnections>();
-    wellCon->add(createConnection(0,0,1)); // {level 0, cell idx 2} -> ijk = {0,0,1}
-    wellCon->add(createConnection(0,0,2)); // {level 0, cell idx 4} -> ijk = {0,0,2}
-
-
-    std::vector<Dune::cpgrid::OpmWellType> wells;
-    wells.push_back(createWell("well1"));
-    wells[0].updateConnections(wellCon,true);
-
-
-    // refined cells leaf indices [6 7 8 9] and [14 15 16 17] -> well2
-    // (parent cells level zero with indices 3 and 5 -> well2)
-    wellCon = std::make_shared<Opm::WellConnections>(); // reset
-    wellCon->add(createConnection(0,1,1)); // {level 0, cell idx 3} -> ijk = {0,1,1}
-    wellCon->add(createConnection(0,1,2)); // {level 0, cell idx 5} -> ijk = {0,1,2}
-
-    wells.push_back(createWell("well2"));
-    wells[1].updateConnections(wellCon,true);
-
-    if (grid.comm().size()>1) {
-
-        grid.loadBalance(&wells,
-                         /* possibleFutureConnections = */ {},
-                         /* transmissibilities = */ nullptr,
-                         /* overlapLayers = */ 1,
-                         /* partitionMethod = */ Dune::PartitionMethod::zoltanGoG,
-                         /* level = */ 0);
-
-    }
-}
-
-BOOST_AUTO_TEST_CASE(loadBalance_grid_with_lgrs_and_coarseAndRefinedCellWells)
-{
-    // create a grid with lgrs and wells
-    Dune::CpGrid grid;
-    grid.createCartesian(/* grid_dims = */ {1,2,4}, /* size = */ {1.,1.,1.});
-
-    grid.addLgrsUpdateLeafView( /* cells_per_dim_vec = */ {{2,2,1}},
-                                /* startIJK_vec = */ {{0,0,1}},
-                                /* endIJK_vec = */ {{1,2,3}},
-                                /* lgr_name_vec = */ {"LGR1"});
-
-    // level zero grid view
-    //
-    // k = 0   1   | k = 1  [3]  | k = 2   [5]   | k = 3   7   |
-    //         0   |        [2]  |         [4]   |         6   |
-    //
-
-    // leaf grid view (LGR1 parent cell indices = {2,3,4,5}, LGR1 dimension 2x4x2)
-    //
-    // k = 0   1   | k = 1  [6 7 8 9]  | k = 2   [14 15 16 17]   | k = 3   19  |
-    //         0   |        [2 3 4 5]  |         [10 11 12 13]   |         18  |
-
-    // coarse cell leaf index 1 and refined cells leaf indices [6 7 8 9] -> well1
-    // (equivalent cell in level zero with index 1 and parent cell with index 3 -> well1)
     auto wellCon = std::make_shared<Opm::WellConnections>();
     wellCon->add(createConnection(0,1,0)); // {level 0, cell idx 1} -> ijk = {0,1,0}
     wellCon->add(createConnection(0,1,1)); // {level 0, cell idx 3} -> ijk = {0,1,1}
 
-
     std::vector<Dune::cpgrid::OpmWellType> wells;
     wells.push_back(createWell("well1"));
     wells[0].updateConnections(wellCon,true);
 
-
-    // refined cells leaf indices [10 11 12 13] and coarse cell index 18 -> well2
-    // (parent cell level zero with indices 4 and equivalent cell index 6 -> well2)
     wellCon = std::make_shared<Opm::WellConnections>(); // reset
     wellCon->add(createConnection(0,0,2)); // {level 0, cell idx 4} -> ijk = {0,0,2}
     wellCon->add(createConnection(0,0,3)); // {level 0, cell idx 6} -> ijk = {0,0,3}
@@ -228,28 +107,83 @@ BOOST_AUTO_TEST_CASE(loadBalance_grid_with_lgrs_and_coarseAndRefinedCellWells)
     }
 }
 
+BOOST_AUTO_TEST_CASE(add_wells_and_loadBalance_level_zero_of_cpgrid_with_lgrs_created_from_deck){
+    const std::string deck_string = R"(
+RUNSPEC
+DIMENS
+  3 3 4 /
+GRID
+CARFIN
+-- NAME  I1-I2 J1-J2 K1-K2 NX NY NZ
+'LGR1'   2  3  2  3  1  2  4  4  4/
+ENDFIN
+DX
+  36*1000 /
+DY
+	36*1000 /
+DZ
+	36*20 /
+TOPS
+	36*8325 /
+ ACTNUM
+1 1 1
+1 1 1
+1 1 1
 
-BOOST_AUTO_TEST_CASE(loadBalance_grid_with_lgrs_and_oneCellWells)
-{
-    // create a grid with lgrs and wells
+1 1 1
+1 1 1
+1 1 1
+
+1 1 1
+1 1 1
+1 1 1
+
+1 1 1
+1 1 1
+1 1 1
+ /
+PORO
+  36*0.15 /
+PERMX
+  36*1 /
+COPY
+  PERMX PERMZ /
+  PERMX PERMY /
+/
+EDIT
+OIL
+GAS
+TITLE
+The title
+START
+16 JUN 1988 /
+PROPS
+REGIONS
+SOLUTION
+SCHEDULE
+)";
+
     Dune::CpGrid grid;
-    grid.createCartesian(/* grid_dims = */ {1,2,4}, /* size = */ {1.,1.,1.});
+    Opm::createGridAndAddLgrs(grid,
+                              deck_string,
+                              /* cells_per_dim_vec = */ {{2,2,2}},
+                              /* startIJK_vec = */ {{1,1,0}},
+                              /* endIJK_vec = */ {{3,3,2}},
+                              /* lgr_name_vec = */ {"LGR1"});
 
-    grid.addLgrsUpdateLeafView( /* cells_per_dim_vec = */ {{2,2,1}},
-                                    /* startIJK_vec = */ {{0,0,1}},
-                                    /* endIJK_vec = */ {{1,2,3}},
-                                    /* lgr_name_vec = */ {"LGR1"});
-     
-     // refined cells leaf indices [6 7 8 9] (children of {level 0, cell idx 3})-> well1
     auto wellCon = std::make_shared<Opm::WellConnections>();
-    wellCon->add(createConnection(0,1,1)); // {level 0, cell idx 3} -> ijk = {0,1,1}
+    wellCon->add(createConnection(0,1,1)); // {level 0, cell idx 12} -> ijk = {0,1,1}
+    wellCon->add(createConnection(0,1,2)); // {level 0, cell idx 21} -> ijk = {0,1,2}
+    wellCon->add(createConnection(0,1,3)); // {level 0, cell idx 30} -> ijk = {0,1,3}
 
     std::vector<Dune::cpgrid::OpmWellType> wells;
     wells.push_back(createWell("first"));
     wells[0].updateConnections(wellCon,true);
 
     wellCon = std::make_shared<Opm::WellConnections>(); // reset
-    wellCon->add(createConnection(0,0,3)); // {level 0, cell idx 6} -> ijk = {0,0,3}
+    wellCon->add(createConnection(2,1,1)); // {level 0, cell idx 14} -> ijk = {2,1,1}
+    wellCon->add(createConnection(2,1,2)); // {level 0, cell idx 23} -> ijk = {2,1,2}
+    wellCon->add(createConnection(2,1,3)); // {level 0, cell idx 32} -> ijk = {2,1,3}
 
     wells.push_back(createWell("second"));
     wells[1].updateConnections(wellCon,true);
@@ -264,6 +198,7 @@ BOOST_AUTO_TEST_CASE(loadBalance_grid_with_lgrs_and_oneCellWells)
                          /* level = */ 0);
 
     }
+
 }
 
 BOOST_AUTO_TEST_CASE(futureConnection_in_grid_with_lgrs, *boost::unit_test::disabled())
@@ -273,9 +208,9 @@ BOOST_AUTO_TEST_CASE(futureConnection_in_grid_with_lgrs, *boost::unit_test::disa
     grid.createCartesian(/* grid_dims = */ {1,2,4}, /* size = */ {1.,1.,1.});
 
     grid.addLgrsUpdateLeafView( /* cells_per_dim_vec = */ {{2,2,1}},
-                                    /* startIJK_vec = */ {{0,0,1}},
-                                    /* endIJK_vec = */ {{1,2,3}},
-                                    /* lgr_name_vec = */ {"LGR1"});
+                                /* startIJK_vec = */ {{0,0,1}},
+                                /* endIJK_vec = */ {{1,2,3}},
+                                /* lgr_name_vec = */ {"LGR1"});
 
     auto wellCon = std::make_shared<Opm::WellConnections>();
     wellCon->add(createConnection(0,0,0));
@@ -292,12 +227,12 @@ BOOST_AUTO_TEST_CASE(futureConnection_in_grid_with_lgrs, *boost::unit_test::disa
     wells[1].updateConnections(wellCon,true);
 
     wells.push_back(createWell("third"));
-    
+
     std::unordered_map<std::string, std::set<int>> futureConnections;
     futureConnections.emplace("third",std::set<int>{6,7,8,9});
     Dune::cpgrid::WellConnections wellConnections(wells,futureConnections,grid);
 
-     if (grid.comm().size()>1) {
+    if (grid.comm().size()>1) {
 
         grid.loadBalance(&wells, futureConnections,
                          /* transmissibilities = */ nullptr,
@@ -307,8 +242,6 @@ BOOST_AUTO_TEST_CASE(futureConnection_in_grid_with_lgrs, *boost::unit_test::disa
 
     }
 }
-
-
 
 bool
 init_unit_test_func()
