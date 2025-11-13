8117a58f51ddf1720039ae1b9e80001d85ece419
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2025-11-13 14:28:52 +0100

    Make entity marking dune conformant
    Ensures that the entity marking process conforms to dune interface by adding a boolean flag that defines whether to throw an exception when the marking is invalid or not. If false, the function returns gracefully as the dune-grid interface expects. If true, the function will throw a message with the reason of the failure.

--- tests/cpgrid/lgr/autoRefine_test.cpp
+++ tests/cpgrid/lgr/autoRefine_test.cpp
@@ -134,7 +134,7 @@ BOOST_AUTO_TEST_CASE(callAdaptAfterAutoRefine_serial) {
         checkGridAfterAutoRefinement(grid, /* nxnynz = */ {3,3,1});
 
         for (const auto& element : Dune::elements(grid.leafGridView())) {
-            grid.mark(1, element);
+            grid.mark(1, element, true);
         }
         grid.preAdapt();
         grid.adapt();
@@ -167,7 +167,7 @@ BOOST_AUTO_TEST_CASE(callAdaptNotAllElementsMarkedAfterAutoRefine_serial) {
             const int id = grid.globalIdSet().id(element);
             // Mark two elements
             if ( (id == maxCellId - 2) || (id == maxCellId -1)) {
-                grid.mark(1, element);
+                grid.mark(1, element, true);
             }
         }
         grid.preAdapt();
