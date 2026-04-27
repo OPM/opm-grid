281fefd0643a3929c67cf3d808d0b9e1346480d4
Antonella Ritorto <anto.ritorto@gmail.com>
2026-04-27 12:56:40 +0200

    Make consistent the vertex order in face_to_point for CpGrid with LGRs

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -112,6 +112,7 @@ list(APPEND TEST_SOURCE_FILES
   tests/cpgrid/lgr/addLgrs_in_allActiveCartesianGrid_test.cpp
   tests/cpgrid/lgr/addLgrsOnDistributedGrid_test.cpp
   tests/cpgrid/lgr/autoRefine_test.cpp
+  tests/cpgrid/lgr/consistent_vertex_order_in_face_test.cpp
   tests/cpgrid/lgr/global_refine_test.cpp
   tests/cpgrid/lgr/id_entity_entityrep_test.cpp
   tests/cpgrid/lgr/level_and_grid_cartesianIndexMappers_test.cpp
